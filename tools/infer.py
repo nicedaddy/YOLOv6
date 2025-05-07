cbcad152c85e67386716e8528f800bc1a23afeec
nicedaddy <pasu71@gmail.com>
2025-05-07 15:59:25 +0900

    test commit

--- tools/infer.py
+++ tools/infer.py
@@ -23,6 +23,7 @@ import torch
 ROOT = os.getcwd()
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
+    # test commit
 
 from yolov6.utils.events import LOGGER
 from yolov6.core.inferer import Inferer
